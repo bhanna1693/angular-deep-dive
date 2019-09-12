8f5f2339fc8f0f88f7bebf89980a8107a9ce6f4e
bhanna <bhanna1693@gmail.com>
2019-09-11 21:52:18 -0400

    open sidenav button moved to navbar and created a navigation service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationService} from './services/navigation.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isSideNavOpen = false;
+
+  constructor(private navService: NavigationService) {
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavigationService} from '../services/navigation.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private navService: NavigationService) { }
 
   ngOnInit() {
   }
