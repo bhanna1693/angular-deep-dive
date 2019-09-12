336c47df87a601040bc5f3e912183854ac3183da
bhanna <bhanna1693@gmail.com>
2019-09-11 22:26:18 -0400

    further routing added to help with flow of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NavComponent} from './nav/nav.component';
 import {MaterialModule} from './material/material.module';
 import {FooterComponent} from './footer/footer.component';
-import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { AboutInvestingComponent } from './about-investing/about-investing.component';
+import {AboutInvestingComponent} from './about-investing/about-investing.component';
 import {AppRoutingModule} from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
